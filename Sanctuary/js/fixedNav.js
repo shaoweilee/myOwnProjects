9c5e119aae9f21c67f64af528c58a30bc7ef88dc
shaoweilee <610382941@qq.com>
2017-04-18 18:24:59 +0800

    add nav events

---
+++ Sanctuary/js/fixedNav.js
@@ -0,0 +1,39 @@
+$.fixedNav = function(){
+	var nav = document.getElementsByClassName("navbar")[0];
+//	var navHeight = $(nav).height();
+//	$(window).scrollTop(300);
+	$(window).scroll(function(){
+		var flag = $(this).scrollTop() <= nav.offsetTop + nav.clientHeight;
+		if (!flag) {
+			$(nav).css({
+				"position": "fixed",
+				"top": 0,
+				"background": "rgb(255, 255, 255)",
+				"transition": "all .3s",
+				"paddingTop": 0
+			});
+			$(".navAlt").css({
+				"height": nav.clientHeight + 33
+			});
+		} else{
+			$(nav).css({
+				"position": "static",
+				"background": "",
+				"paddingTop": 33
+			});
+			$(".navAlt").css({
+				"height": 0
+			});
+		}
+	})
+}
+$.navClick = function(){
+	var navList = $("a[data-hover]", document.getElementsByClassName("navbar")[0]);
+	var clickHandler = function(event, x){
+		var body = document.body;
+		var e = event || window.event;
+		e.preventDefault();
+		body.scrollTop = x;
+	}
+	navList[0].onclick = function(event){clickHandler(event, 0);}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+}
--- Sanctuary/js/index.js
+++ Sanctuary/js/index.js
@@ -1,25 +1,2 @@
-function requestImg(){
-	var p = new Promise(function(resolve, reject){
-		var img = new Image();
-		img.onload = function(){
-			resolve(img);
-		}
-		img.src = "******";
-	});
-	return p;
-}
-function timeout(){
-	var p = new Promise(function(resolve, reject){
-		setTimeout(function(){
-			reject("图片请求超时");
-		}, 5000);
-	});
-	return p;
-}
-Promise.race([requestImg(), timeout()])
-.then(function(result){
-	console.log(result);
-})
-.catch(function(reason){
-	console.log(reason);
-});
+$.fixedNav();
+$.navClick();
