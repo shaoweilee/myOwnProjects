074b3bd8e84618851f1b1a2e702a8bc91785e4ec
shaoweilee <610382941@qq.com>
2017-04-23 22:54:15 +0800

    halfDone easeMove

--- Sanctuary/js/fixedNav.js
+++ Sanctuary/js/fixedNav.js
@@ -5,6 +5,10 @@ $.fixedNav = function(){
 	$(window).scroll(function(){
 		var flag = $(this).scrollTop() <= nav.offsetTop + nav.clientHeight;
 		if (!flag) {
+			$(".navAlt").css({//不在视口内
+				"height": $(nav).height(),
+				"paddingTop": 33
+			});
 			$(nav).css({
 				"position": "fixed",
 				"top": 0,
@@ -12,28 +16,55 @@ $.fixedNav = function(){
 				"transition": "all .3s",
 				"paddingTop": 0
 			});
+		} else{//在视口内
 			$(".navAlt").css({
-				"height": nav.clientHeight + 33
+				"height": 0,
+				"paddingTop": 0
 			});
-		} else{
 			$(nav).css({
 				"position": "static",
 				"background": "",
 				"paddingTop": 33
-			});
-			$(".navAlt").css({
-				"height": 0
 			});
 		}
 	})
 }
 $.navClick = function(){
 	var navList = $("a[data-hover]", document.getElementsByClassName("navbar")[0]);
-	var clickHandler = function(event, x){
+	var clickHandler = function(event, start, end, rate){
 		var body = document.body;
 		var e = event || window.event;
 		e.preventDefault();
-		body.scrollTop = x;
+		ease(start, end, rate, function(value){body.scrollTop = value;console.log("end:"+value,"当前位置"+start);});
 	}
-	navList[0].onclick = function(event){clickHandler(event, 0);}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+//	var clickHandler = function(event, x){
+//		var body = document.body;
+//		var e = event || window.event;
+//		e.preventDefault();
+//		body.scrollTop = x;
+//	}
+	navList[0].onclick = function(event){clickHandler(event, document.body.scrollTop, 0, 6);}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+	navList[1].onclick = function(event){clickHandler(event, document.body.scrollTop, 0, 6);}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+	navList[2].onclick = function(event){clickHandler(event, document.body.scrollTop, 0, 6);}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+	navList[3].onclick = function(event){
+		clickHandler(event, document.body.scrollTop, $(".work h1").offset().top - $(".work header").height() - $(navList).height(), 6);
+	}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+	navList[5].onclick = function(event){clickHandler(event, document.body.scrollTop, 1500, 6);}//注意这里event对象的传递,必须用匿名函数包裹，匿名函数不传参也可以的。
+}
+
+
+var ease = function(start, end, rate, callback){
+	if (start === end) {
+		return;
+	}
+	var step = function(){
+		start = start + (end - start)/rate;
+		if (Math.abs(end - start) < 1) {
+			callback(end, true);
+			return;
+		}
+		callback(start, false);
+		requestAnimationFrame(step);
+	}
+	step();
 }
